ef3cd21058458177b50ea68a16eb37d55f9bb866
jlu51 <jlu51@csc301x07.ad.calpoly.edu>
2019-04-10 09:48:57 -0700

    update 2

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -5,11 +5,18 @@ from lab1 import *
 class TestLab1(unittest.TestCase):
 
     def test_max_list_iter(self):
-        """add description here"""
+        #Checks for the exception
         tlist = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
+	#Tests if list is empty
+        self.assertEqual(max_list_iter([]), None)
+	
+	#Tests if list exists
+        tlist = [5, 2, 4, 6, 1]
+        self.assertEqual(max_list_iter(tlist), 6)
+	
     def test_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
 
@@ -20,6 +27,22 @@ class TestLab1(unittest.TestCase):
         self.assertEqual(bin_search(4, low, len(list_val)-1, list_val), 3)
         self.assertEqual(bin_search(1, low, len(list_val) - 1, list_val), 0)
         self.assertEqual(bin_search(0, low, len(list_val) - 1, list_val), None)
+        
+
+        #Left side
+	self.assertEqual(bin_search(4, low, len(list_val)-1, list_val), 3)
+
+        #Right side
+        self.assertEqual(bin_search(9, low, len(list_val)-1, list_val), 6)
+
+        #Checks for the exception
+        tlist = None
+        with self.assertRaises(ValueError):
+            bin_search(5, low, high, tlist)
+        
+        #Right side
+        self.assertEqual(bin_search(9, low, len(list_val)-1, list_val), 6)
+
 if __name__ == "__main__":
         unittest.main()
 
